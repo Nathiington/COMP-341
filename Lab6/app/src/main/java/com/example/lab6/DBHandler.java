49f7269e86c00f0cbeb9bc67079b53e977df0fc6
Nathan <58734424+Nathiington@users.noreply.github.com>
2021-05-22 20:12:05 +0200

    Lab6 Date and Time Pickers Saving to a SQLite database

---
+++ Lab6/app/src/main/java/com/example/lab6/DBHandler.java
@@ -0,0 +1,67 @@
+package com.example.lab6;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHandler extends SQLiteOpenHelper {
+
+    //variable for database name
+    private static final String DB_NAME = "myDatabase.db";
+
+    //the database version
+    private static final int DB_VERSION = 1;
+
+    // table name in our database
+    private static final String TABLE_NAME ="datetime";
+
+    //variable for date column
+    private static final String DATE_COL = "Date";
+
+    // variable for time column
+    private static final String TIME_COL = "Time";
+
+    // database handler constructor
+    public DBHandler(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    // overriding the inCreate method
+    // this will create a table in our database and execute that specified sql statement (in this case create table)
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String query = "CREATE TABLE " + TABLE_NAME + " (" + DATE_COL + " TEXT, " + TIME_COL + " TEXT ) ";
+
+        db.execSQL(query);
+
+    }
+
+    // this method adds date and time to our sqlite table
+    // we use ContentValues as a map with the key being our table column name and our value being the date or time
+    // we also create an instance of an abstract function SQLiteDatabase which we will use to insert data into our database
+    public void addDateTime(String cdate, String ctime)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues cvalues = new ContentValues();
+        cvalues.put(DATE_COL, cdate);
+        cvalues.put(TIME_COL, ctime);
+
+        db.insert(TABLE_NAME,null, cvalues);
+        db.close();
+
+    }
+
+    // this would be used if we need to re-write the table so we drop the current version and add the new one
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+
+    }
+}
---
+++ Lab6/app/src/main/java/com/example/lab6/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button dateBtn,timeBtn,saveBtn;
+    EditText editTime,editDate;
+    static int minutes,hours,day,months,years;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DBHandler hand = new DBHandler(MainActivity.this);
+
+
+
+        editTime = findViewById(R.id.editTextTime);
+        timeBtn = findViewById(R.id.timeButton);
+        timeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // getting an instance of an abstract Calendar object
+                Calendar myCalendar = Calendar.getInstance();
+                // getting the minutes and hours from the calendar object
+                minutes = myCalendar.get(Calendar.MINUTE);
+                hours = myCalendar.get(Calendar.HOUR);
+
+                // we create a TimePickerDialog object with the parameters of context and TimePickerDialog.OnTimeSetListener() method
+                // we pass the TimePickerDialog.OnTimeSetListener() method as a parameter so we need to override the inbuilt function and use our own data
+                // the onTimeSet method will create a timepicker view that we show at the end
+                // it also sets the data to our adjacent EditText so we can see which Date we selected
+                TimePickerDialog t = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        editTime.setText(hourOfDay + ":" + minute);
+                    }
+                },hours,minutes,false);
+                t.show();
+            }
+        });
+
+
+        editDate = findViewById(R.id.editTextDate);
+        dateBtn =findViewById(R.id.dateButton);
+        dateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar myCalendar = Calendar.getInstance();
+                day = myCalendar.get(Calendar.DAY_OF_MONTH);
+                months = myCalendar.get(Calendar.MONTH);
+                years = myCalendar.get(Calendar.YEAR);
+
+                DatePickerDialog d = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        month=month+1;
+                        editDate.setText(dayOfMonth+"/"+month+"/"+year);
+                    }
+                },years,months,day);
+                d.show();
+            }
+        });
+
+
+        saveBtn = findViewById(R.id.saveButton);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // getting the data from the EditText widgets
+                String myDate = editDate.getText().toString();
+                String myTime = editTime.getText().toString();
+
+                // if date and time is empty then do not update the database table at all
+                if(myDate.isEmpty() && myTime.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"No Date/Time",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                // otherwise add date and time that we get from the EditText widgets above in the method
+                    hand.addDateTime(myDate,myTime);
+                    Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_LONG).show();
+                    // clearing the EditText widgets so new data can be easily input
+                    editDate.setText("");
+                    editTime.setText("");
+            }
+        });
+    }
+}
